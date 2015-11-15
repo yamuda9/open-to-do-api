c8fcd03502ca23ca31a2e2f56b59838cb346d302
Conrad Taylor <conradwt@gmail.com>
2015-11-14 16:02:23 -0800

    Change password_digest to password.

--- db/schema.rb
+++ db/schema.rb
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20151113081416) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "auth_token"
   end
 
