2ac9452846d282f8b630eb20969ea05f1480a8dd
Conrad Taylor <conradwt@gmail.com>
2015-11-14 16:20:36 -0800

    Change password_digest to password.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,7 +5,7 @@ class ApiController < ApplicationController
 private
 
   def authenticated?
-    authenticate_or_request_with_http_basic { | username, password | User.where( username: username, password_digest: password ).present? }
+    authenticate_or_request_with_http_basic { | username, password | User.where( username: username, password: password ).present? }
   end
   
 end
